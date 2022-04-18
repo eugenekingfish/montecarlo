a1830af261f0e43ccb349832ad4bc50d89097c12
eugene <eugene@pop-os.localdomain>
2022-04-18 18:40:47 +0100

    added points2d.py, removed monte_carlo.py

--- points2d.py
+++ points2d.py
@@ -1,9 +1,8 @@
-import random
 from matplotlib import pyplot as plt
 import numpy as np
-import math
+from math import pi
 
-class Points:
+class Points2D:
     def __init__(self, n):
         self.n = n
         self.points = np.array([[np.random.uniform(0.0, 1.0, 1), np.random.uniform(0.0, 1.0, 1)] for _ in range(n)])
@@ -29,7 +28,7 @@ class Points:
         cols = []
         for distance in self.distances:
             if distance <= 1:
-                cols.append( (0,1,0.5,1) )
+                cols.append( (0,0,1,1) )
             else:
                 cols.append( (1,0,0,1) )
         return cols
@@ -37,18 +36,19 @@ class Points:
     def plot(self):
         cols = self._gen_colors()
         figure, axes = plt.subplots()
+
+        axes.set_xlim(0.0,1.0)
+        axes.set_ylim(0.0,1.0)
+        axes.set_xlabel("x")
+        axes.set_ylabel("y")
+
+        approx = self.approximate() * 4
+        error = ((pi - approx) / pi)*100
+        title = "π ≈ " + str(approx) + "\n n = " + str(len(self.points)) + "\n error = " + str(error) + "%"
+
+        axes.set_title(title)
         plt.scatter(self.points[:,0], self.points[:,1],s=3,c=cols)
-        circ = plt.Circle((0,0), 1, fill=False,color=(0,0,0,0.25))
+        circ = plt.Circle((0,0), 1, color=(0,0,0,0.25))
         axes.add_artist(circ)
         plt.show()
         return None
-
-pts = Points(100000)
-print(pts.approximate()*4)
-pts.plot()
-
-
-
-
-
-
